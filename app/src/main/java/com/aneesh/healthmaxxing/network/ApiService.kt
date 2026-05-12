1aab8703b48f5fd631820d7ad9c86c3ed0aa5fdd
aneeshpatne <aneeshpatne@gmail.com>
2026-05-12 12:14:24 +0530

    Configure network security settings and define initial API endpoint
    - Add `network_security_config.xml` to permit cleartext traffic using system trust anchors
    - Define a new `test` GET endpoint in the `ApiService` interface for connectivity testing

--- app/src/main/java/com/aneesh/healthmaxxing/network/ApiService.kt
+++ app/src/main/java/com/aneesh/healthmaxxing/network/ApiService.kt
@@ -1,5 +1,11 @@
 package com.aneesh.healthmaxxing.network
 
+import okhttp3.ResponseBody
+import retrofit2.http.GET
+
+
 interface ApiService {
+    @GET("/client/start")
+    suspend fun test(): ResponseBody
 
 }
