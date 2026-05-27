a437492d489422e3e951db33360c9fd14a7659a6
aneeshpatne <aneeshpatne@gmail.com>
2026-05-27 16:30:51 +0530

    Center align text elements in LoginPage2

--- app/src/main/java/com/aneesh/healthmaxxing/ui/login/LoginPage2.kt
+++ app/src/main/java/com/aneesh/healthmaxxing/ui/login/LoginPage2.kt
@@ -5,10 +5,17 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.aneesh.healthmaxxing.R
 
 @Composable
 fun LoginPage2() {
@@ -21,6 +28,21 @@ fun LoginPage2() {
         verticalArrangement = Arrangement.SpaceBetween
     ) {
         FormaHeader()
-
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            Text(
+                "Let's get to know you",
+                fontSize = 30.sp,
+                fontFamily = FontFamily(Font(R.font.cormorant_garamond_variablefont_wght)),
+                fontWeight = FontWeight.SemiBold,
+                textAlign = TextAlign.Center
+            )
+            Text(
+                text = "Add your name and email to create your account",
+                textAlign = TextAlign.Center
+            )
+        }
     }
 }
