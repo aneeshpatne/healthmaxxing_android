868793f4736e159e5f52ee79fcbc493d1bff6465
aneeshpatne <aneeshpatne@gmail.com>
2026-05-28 12:42:19 +0530

    Set up Retrofit with Hilt DI and update ApiService to use register DTOs

--- app/src/main/java/com/aneesh/healthmaxxing/data/remote/ApiService.kt
+++ app/src/main/java/com/aneesh/healthmaxxing/data/remote/ApiService.kt
@@ -1,11 +1,9 @@
 package com.aneesh.healthmaxxing.data.remote
 
-import okhttp3.RequestBody
-import okhttp3.Response
 import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface ApiService {
     @POST("register")
-    suspend fun register(@Body request: RequestBody): Response
+    suspend fun register(@Body request: RegisterRequest): RegisterResponse
 }
---
+++ app/src/main/java/com/aneesh/healthmaxxing/data/remote/NetworkModule.kt
@@ -0,0 +1,27 @@
+package com.aneesh.healthmaxxing.data.remote
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+    @Provides
+    @Singleton
+    fun provideRetrofit(): Retrofit {
+        return Retrofit.Builder().baseUrl("http://192.168.0.99:3030/").addConverterFactory(
+            GsonConverterFactory.create()
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideApiService(retrofit: Retrofit): ApiService {
+        return retrofit.create(ApiService::class.java)
+    }
+}
