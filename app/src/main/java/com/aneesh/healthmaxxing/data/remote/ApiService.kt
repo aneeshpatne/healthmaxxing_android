0c72340f3f00878d6406a16342e772340d5f245c
aneeshpatne <aneeshpatne@gmail.com>
2026-05-28 17:07:46 +0530

    Wrap register endpoint response in Retrofit Response type

--- app/src/main/java/com/aneesh/healthmaxxing/data/remote/ApiService.kt
+++ app/src/main/java/com/aneesh/healthmaxxing/data/remote/ApiService.kt
@@ -1,9 +1,10 @@
 package com.aneesh.healthmaxxing.data.remote
 
+import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface ApiService {
     @POST("register")
-    suspend fun register(@Body request: RegisterRequest): RegisterResponse
+    suspend fun register(@Body request: RegisterRequest): Response<RegisterResponse>
 }
