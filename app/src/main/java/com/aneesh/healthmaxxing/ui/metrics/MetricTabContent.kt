42f96685395e9562c645d8b9c1aba276214f4c24
aneeshpatne <aneeshpatne@gmail.com>
2026-05-30 17:19:07 +0530

    feat(metrics): create blank MetricTabContent component

---
+++ app/src/main/java/com/aneesh/healthmaxxing/ui/metrics/MetricTabContent.kt
@@ -0,0 +1,15 @@
+package com.aneesh.healthmaxxing.ui.metrics
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun MetricTabContent(
+    selectedTab: Int,
+    modifier: Modifier = Modifier
+) {
+    Box(modifier = modifier) {
+        
+    }
+}
